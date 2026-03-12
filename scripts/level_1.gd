25d5fd2788ad63ffbeb77b59934a132108fdc956
Paul Adam <adamp@posteo.de>
2026-03-12 13:36:44 +0100

    add make_item function to easily change items around
    Signed-off-by: Paul Adam <adamp@posteo.de>

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const CUTE_SPIDER = preload("uid://bnn03bt1lwb0g")
+const DRAG = preload("uid://dkfhmnuke066g")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,13 +23,17 @@ func _on_water_drop_interaction(from: Node2D) -> void:
 	from.fade_away()
 
 
-func _on_bow_interaction() -> void:
+func _on_bow_interaction(form: Node2D) -> void:
 	$Bow/AudioStreamPlayer.stop()
 	$Spider/AudioStreamPlayer2.play()
 	
-	var cute_spider = CUTE_SPIDER.instantiate()
+	var cute_spider = make_item("res://assets/ElinAssets/CuteSpider.png")
 	cute_spider.global_position = $Spider.global_position + Vector2(0, 50)
 	add_child(cute_spider)
+	
+	#var cute_spider = CUTE_SPIDER.instantiate()
+	#cute_spider.global_position = $Spider.global_position + Vector2(0, 50)
+	#add_child(cute_spider)
 	
 	$Spider.hide()
 	$Bow.hide()
@@ -43,3 +48,17 @@ func _on_player_died() -> void:
 
 func _on_player_end_level() -> void:
 	reset()
+
+func make_item(
+	texture_path: String,
+	draggable: bool = false,
+	interact_with: String = "",
+	sounds: Array[AudioStream] = [],
+	) -> Node2D:
+	var texture = load(texture_path)
+	var item = DRAG.instantiate()
+	item.texture = texture
+	item.draggable = draggable
+	item.interact_with = interact_with
+	item.sounds = sounds
+	return item

d8a468ddec092a260107dcc24406dde07d14e90f
Florian Burger <florianburger03@gmail.com>
2026-03-12 10:35:37 +0100

    add idle animation for draggable items

---
+++ scripts/idle_animation.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	while get_parent().draggable:
+		await get_tree().create_timer(randfn(3, 10)).timeout
+		_bounce()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _bounce():
+	var tween := create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(get_parent(), "scale", Vector2.ONE * 1.1, 0.15)
+	tween.chain()
+	tween.tween_property(get_parent(), "scale", Vector2.ONE, 0.15)

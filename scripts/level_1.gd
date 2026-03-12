81e0a7fe3347bcf2e7efd2776e1e1e8a7e2066dc
cefoxo4 <cefoxo4@gmail.com>
2026-03-12 15:03:19 +0100

    added simple interactions

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const CUTE_SPIDER = preload("uid://bnn03bt1lwb0g")
 const DRAG = preload("uid://dkfhmnuke066g")
+var bow_used = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,21 +26,29 @@ func _on_water_drop_interaction(from: Node2D, _with: Node2D) -> void:
 
 func _on_bow_interaction(from: Node2D, _with: Node2D) -> void:
 	$Bow/AudioStreamPlayer.stop()
-	$Spider/AudioStreamPlayer2.play()
-	
-	var cute_spider = make_item("res://assets/ElinAssets/CuteSpider.png")
-	cute_spider.global_position = $Spider.global_position + Vector2(0, 50)
-	add_child(cute_spider)
-	
-	#var cute_spider = CUTE_SPIDER.instantiate()
-	#cute_spider.global_position = $Spider.global_position + Vector2(0, 50)
-	#add_child(cute_spider)
-	
-	$Spider.hide()
-	$Bow.hide()
-	await $Spider/AudioStreamPlayer2.finished
-	$Bow.queue_free()
-	$Spider.queue_free()
+	if _with == $Spider and not bow_used:
+		bow_used = true
+		$Spider/AudioStreamPlayer2.play()
+		
+		var cute_spider = make_item("res://assets/ElinAssets/CuteSpider.png")
+		cute_spider.global_position = $Spider.global_position + Vector2(0, 50)
+		add_child(cute_spider)
+		
+		#var cute_spider = CUTE_SPIDER.instantiate()
+		#cute_spider.global_position = $Spider.global_position + Vector2(0, 50)
+		#add_child(cute_spider)
+		
+		$Spider.hide()
+		$Bow.hide()
+		await $Spider/AudioStreamPlayer2.finished
+		$Bow.queue_free()
+		$Spider.queue_free()
+	if _with == $Fire and not bow_used:
+		bow_used = true
+		var BurntBow = make_item("res://assets/ElinAssets/BowBurned.png",true)
+		BurntBow.global_position = from.global_position
+		add_child(BurntBow)
+		from.queue_free()
 
 
 func _on_player_died() -> void:
@@ -62,3 +71,53 @@ func make_item(
 	item.interact_with = interact_with
 	item.sounds = sounds
 	return item
+
+
+func _on_ice_cream_interaction(from: Node2D, with: Node2D) -> void:
+	$IceCream/AudioStreamPlayer.stop()
+	var IceCreamBite = make_item("res://assets/ElinAssets/IceCreamBite.png",true)
+	IceCreamBite.global_position = from.global_position
+	add_child(IceCreamBite)
+	from.queue_free()
+
+
+func _on_butterfly_interaction(from: Node2D, with: Node2D) -> void:
+	$Butterfly/AudioStreamPlayer.stop()
+	var BurnButterfly = make_item("res://assets/ElinAssets/ButterflyBurning.png",true)
+	BurnButterfly.global_position = from.global_position
+	add_child(BurnButterfly)
+	from.queue_free()
+
+
+func _on_muffin_interaction(from: Node2D, with: Node2D) -> void:
+	$Muffin/AudioStreamPlayer.stop()
+	var BurntMuffin = make_item("res://assets/ElinAssets/MuffinBurned.png",true)
+	BurntMuffin.global_position = from.global_position
+	add_child(BurntMuffin)
+	from.queue_free()
+
+
+func _on_star_interaction(from: Node2D, with: Node2D) -> void:
+	$Star/AudioStreamPlayer.stop()
+	var AngryStar = make_item("res://assets/ElinAssets/StarHurt.png",true)
+	AngryStar.global_position = from.global_position
+	add_child(AngryStar)
+	from.queue_free()
+
+
+func _on_balloons_interaction(from: Node2D, with: Node2D) -> void:
+	$Balloons/AudioStreamPlayer.stop()
+	var ClownBalloon = make_item("res://assets/ElinAssets/ClownBalloon.png")
+	ClownBalloon.global_position = with.global_position
+	add_child(ClownBalloon)
+	from.queue_free()
+	with.queue_free()
+
+
+func _on_soap_interaction(from: Node2D, with: Node2D) -> void:
+	$Soap/AudioStreamPlayer.stop()
+	var ClownNoScary = make_item("res://assets/ElinAssets/ClownNoScary.png")
+	ClownNoScary.global_position = with.global_position
+	add_child(ClownNoScary)
+	from.queue_free()
+	with.queue_free()

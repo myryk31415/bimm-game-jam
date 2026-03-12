15bff671f4cada35840cab7155a33b01e58f286d
Paul Adam <adamp@posteo.de>
2026-03-12 10:54:44 +0100

    add cute spider
    Signed-off-by: Paul Adam <adamp@posteo.de>

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+const CUTE_SPIDER = preload("uid://bnn03bt1lwb0g")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,5 +20,13 @@ func reset():
 
 
 func _on_water_drop_interaction() -> void:
-	remove_child($WaterDrop)
-	remove_child($Fire)
+	$WaterDrop.queue_free()
+	$Fire.queue_free()
+
+
+func _on_bow_interaction() -> void:
+	var cute_spider = CUTE_SPIDER.instantiate()
+	cute_spider.global_position = $Spider.global_position + Vector2(0, 50)
+	add_child(cute_spider)
+	$Spider.queue_free()
+	$Bow.queue_free()
